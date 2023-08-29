70ade0e92365010e9971bc01d3c3f9d9d18e748b
sontrinh2003 <hoangson200403@gmail.com>
2023-08-29 13:35:44 +1000

    Added Dashboard and styles

--- dashboard.php
+++ dashboard.php
@@ -8,43 +8,128 @@
   <meta name="author" content="Group 19" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="styles/style.css" rel="stylesheet" />
+  <link href="./styles/style.css" rel="stylesheet" />
+  <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
   <title>Dashboard Page</title>
   <link rel="icon" href="styles/images/logo.svg" type="image/icon" />
 </head>
 
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
       <a class="navbar-brand" href="#">CityLogistics</a>
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="#">Search by truck <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="#">Search by Microhub <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-success" type="submit">Search</button>
-      </form>
     </div>
-  </nav>
+  </nav> -->
+  <div class="sidebar bg-primary">
+    <div class="logo-details">
+      <i class="bx bx-sitemap"></i>
+      <span class="logo_name">CityLogistics</span>
+    </div>
+    <ul class="nav-links">
+      <li class="bg-primary">
+        <a href="#" class="active">
+          <i class="bx bxs-dashboard"></i>
+          <span class="links_name">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bxs-truck"></i>
+          <span class="links_name">Search by truck</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-package"></i>
+          <span class="links_name">Search by micro hubs</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-message-rounded-dots'></i>
+          <span class="links_name">Contact</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class="bx bx-menu sidebarBtn"></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+      <div class="search-box">
+        <input type="text" placeholder="Search..." />
+        <i class="bx bx-search"></i>
+      </div>
+      <div class="profile-details">
+        <img src="./assets/images/ava1.jpeg" alt="" />
+        <span class="admin_name">John Doe</span>
+        <i class="bx bx-chevron-down"></i>
+      </div>
+    </nav>
+
+    <div class="home-content">
+      <div class="overview-boxes">
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Average Order Value</div>
+            <div class="number">$306.20</div>
+            <div class="indicator">
+              <i class="bx bx-down-arrow-alt down"></i>
+              <span class="text">1.3% down from last month</span>
+            </div>
+          </div>
+          <i class="bx bx-cart-alt cart"></i>
+        </div>
+
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Number of trucks on road</div>
+            <div class="number">150</div>
+            <div class="indicator">
+              <i class="bx bx-down-arrow-alt down"></i>
+              <span class="text">1.2% down from last month</span>
+            </div>
+          </div>
+          <i class="bx bxs-truck cart" ></i>
+        </div>
+
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Number of micro hubs</div>
+            <div class="number">505</div>
+            <div class="indicator">
+              <i class="bx bx bx-up-arrow-alt"></i>
+              <span class="text">2.3% up from last month</span>
+            </div>
+          </div>
+          <i class="bx bx-package cart"></i>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
   <?php
   include_once("footer.inc");
   ?>
+  <script>
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".sidebarBtn");
+    sidebarBtn.onclick = function() {
+      sidebar.classList.toggle("active");
+      if (sidebar.classList.contains("active")) {
+        sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    };
+  </script>
 </body>
 
 </html>
